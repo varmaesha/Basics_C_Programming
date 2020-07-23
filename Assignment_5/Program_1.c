553415f0cc98b1922748d0e11f288308d81213ec
varmaesha <varmaesha77@gmail.com>
2020-07-23 14:54:51 +0530

    Adding Files To Assignment_5

--- Assignment_5/Program_1.c
+++ Assignment_5/Program_1.c
@@ -1,2 +1,25 @@
 //Write a program which accept one number from user and return smallest digit.
 
+#include"stdio.h"
+
+void retSmallestDig(int);
+
+void main(){
+    int no;
+    printf("Enter no to get smallest digit:");
+    scanf("%d",&no);
+    retSmallestDig(no);
+}
+void retSmallestDig(int no){
+    int temp, smallest = 9;
+    while (no > 0)
+    {
+        temp = no % 10;
+        if(temp <= smallest){
+        smallest = temp;
+        }
+        no = no / 10;
+    }
+    //return smallest;
+    printf("The smallest digit is %d",smallest);
+}
