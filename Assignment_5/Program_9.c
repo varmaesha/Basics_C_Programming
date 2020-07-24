a42864c55370be4a95b5f8889043d6fdf7f4118d
varmaesha <varmaesha77@gmail.com>
2020-07-24 16:07:57 +0530

    Adding Files To Assignment_5

--- Assignment_5/Program_9.c
+++ Assignment_5/Program_9.c
@@ -2,3 +2,17 @@
 
 #include"stdio.h"
 
+void areaOfRect(float,float);
+
+void main(){
+    float length, width;
+    printf("Enter lengeth and width of rectangle:\n");
+    scanf("%f%f",&length,&width);
+    areaOfRect(length,width);
+}
+
+void areaOfRect(float length, float width){
+    float area;
+    area = length * width;
+    printf("The area of rectangle is %.3f",area);
+}
