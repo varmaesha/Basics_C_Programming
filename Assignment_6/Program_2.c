fef319d8422bc9c9133d19a4197850d6bc8b99b9
varmaesha <varmaesha77@gmail.com>
2020-07-26 21:52:09 +0530

    Adding Files To Assignment_6

--- Assignment_6/Program_2.c
+++ Assignment_6/Program_2.c
@@ -1,5 +1,5 @@
 /*
-Parking charges for four whaler is 30 rupees for first three hours and after every hour it is 5 rupees. 
+Parking charges for four wheeler is 30 rupees for first three hours and after every hour it is 5 rupees. 
 Accept number of hours and calculate total amount.
 Input : 2
 Output : 30
@@ -8,4 +8,28 @@ Output : 50
 */
 
 #include"stdio.h"
-#define First3 30
+#define FIRST3 30
+#define AFTER3 5
+
+void totalRent(int);
+
+void main(){
+    int hrs;
+    printf("Input:");
+    scanf("%d",&hrs);
+    totalRent(hrs);
+}
+
+void totalRent(int hrs){
+    int icost = 0,tcost = 0;
+    if(hrs < 4){
+        tcost =FIRST3;
+    }
+    else
+    {
+ 
+        icost =((hrs - 3) *AFTER3);
+        tcost = icost +  FIRST3;
+    }
+    printf("Output: %d",tcost);
+}
