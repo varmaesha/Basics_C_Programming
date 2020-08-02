097bbcc239f5010b700b6080dfbf7926f59f97fa
varmaesha <varmaesha77@gmail.com>
2020-08-02 19:16:06 +0530

    Adding files to Assignment_8

---
+++ Assignment_8/Program_1.c
@@ -0,0 +1,41 @@
+/*.Write a program which accept 10 numbers from user and display only even numbers from them.
+Input : 21 78 32 12 89 67 62 55 33 10
+Output : 78 32 12 62 10
+*/
+
+#include"stdio.h"
+#include"stdlib.h"
+
+void getEvenArray(int[],int);
+
+void getEvenArray(int arr[], int size){
+    int comp;
+    printf("Even Elements of array are:\n");
+    for(comp = 0; comp < size; comp++)
+    {
+        if(arr[comp] % 2 == 0){
+        printf("%d\n",arr[comp]);
+        }
+    }
+}
+
+void main(){
+    int *arr = NULL;
+    int size,comp;
+
+    printf("Enter the number of Elements:");
+    scanf("%d",&size);
+
+    arr = (int *)malloc(sizeof(int)*size);
+
+    for (int comp = 0; comp < size; comp++)
+    {
+        scanf("%d",&arr[comp]);
+    }
+
+    getEvenArray(arr,size);
+    /*
+    */
+    
+}
+
