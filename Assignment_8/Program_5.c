c93f9f81c8c416669615ad0b1110e550993259f3
varmaesha <varmaesha77@gmail.com>
2020-08-04 22:08:33 +0530

    Adding gitignore file

---
+++ Assignment_8/Program_5.c
@@ -0,0 +1,32 @@
+/*Write a program which accept 8 numbers from user and print prime numbers from them.
+Input : 7 45 29 31 71 91 11 28
+Output : 7 29 31 71 11
+*/
+
+#include"stdio.h"
+#include"stdlib.h"
+
+void getPrime(int[],int);
+
+void getPrime(int arr[],int size){
+    for(int i = 0; i < size; i++){
+
+    
+    }
+}
+
+void main(){
+    int *arr = NULL;
+    int size;
+
+    printf("Enter the size:");
+    scanf("%d",&size);
+
+    arr = (int *)malloc(sizeof(int)*size);
+
+    for(int i = 0; i < size; i++){
+        scanf("%d",&arr[i]);
+    }
+
+    getPrime(arr,size);
+}
